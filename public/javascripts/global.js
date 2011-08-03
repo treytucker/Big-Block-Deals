e3165b761e2242e46cfe250e6a6a1bd0b3c96070
Trey Tucker <trey@treytucker.com>
2011-08-03 18:03:57 -0500

    Nav complete. Quotes complete. Main container styled. Midway through sidebar widgets.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-	
+$('#all-quotes').mbScrollable({
+	width:914,
+	elementsInPage:1,
+	controls:"#controls",
+	autoscroll:false,
+});	
+
+$('#quote-closer').click(function (){
+	$('.quote-container').slideToggle("slow");
+})
 
 });
