04bb0b0c353b040488d922098040af69a87cc9f8
Andrew Ellis <Andrew@andrewryanellis.com>
2011-08-15 13:48:45 -0500

    Rigging the quotes...Janky code right here...

--- app/controllers/good_reads_controller.rb
+++ app/controllers/good_reads_controller.rb
@@ -14,6 +14,7 @@ class GoodReadsController < ApplicationController
     @catagories = Catagory.all
     @dates = GoodRead.all( :select => "created_at", :order => "created_at" )
     @layout = "three-fourths"
+    @quotes = nil
   end
 
   def show
--- app/models/good_read.rb
+++ app/models/good_read.rb
@@ -6,13 +6,14 @@ class GoodRead < ActiveRecord::Base
   attr_accessible :post, :title, :catagory_id, :image, :image_delete
   validates_presence_of :post, :title, :catagory, :image, :message => "can't be blank"
   belongs_to :catagory
-  
+  has_many :quotes, :as => :quotable
+  has_many :widgets, :as => :wedgetable
 
 # Relates to the images
   has_attached_file :image, :styles => { :thumb => "100x100>", :medium => "315x315>" }
-  # validates_attachment_size :image, :less_than => 5.megabytes
-  # validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
-  #
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+  
     before_save :destroy_image?
     
     def image_delete
